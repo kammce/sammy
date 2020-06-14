c2bb1bfc7d6936410277eab927f5ccf894896365
Khalil Estell <kammce@google.com>
2020-06-14 13:04:00 -0700

    Make sammy available as a commandline tool
    - Migrate sammy script and __init__.py to sammy folder
    - Added entry_points to make the sammy script accessible as a command
      line tool when installed via pip
    
    In support of issue #1

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from .sammy import sammy
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sammy-sjsu-dev2",
-    version="0.0.2",
+    version="0.0.0",
     author="SJSU-Dev2 Organization",
     description="A tool for managing SJSU-Dev2 firmware projects and to install external packages such as platforms and libraries.",long_description=long_description,
     long_description_content_type="text/markdown",
@@ -17,5 +17,8 @@ setuptools.setup(
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX :: Linux",
     ],
+    entry_points={
+        'console_scripts': ['sammy=sammy.sammy:main'],
+    },
     python_requires='>=3.6',
 )
